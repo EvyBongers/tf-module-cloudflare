4e3035f3d3674d91e87d955b6a5e93d3140236c0
Evy Bongers <evy@evybongers.nl>
2021-05-30 14:08:34 +0200

    Add support for a number of zone settings

--- zone/settings.tf
+++ zone/settings.tf
@@ -2,5 +2,31 @@ resource "cloudflare_zone_settings_override" "settings" {
   zone_id = cloudflare_zone.dns_zone.id
 
   settings {
+    ipv6 = var.zone_settings.ipv6
+
+    always_use_https         = var.zone_settings.always_use_https
+    automatic_https_rewrites = var.zone_settings.automatic_https_rewrites
+    opportunistic_encryption = var.zone_settings.opportunistic_encryption
+
+    ssl             = var.zone_settings.ssl
+    tls_1_3         = var.zone_settings.tls_1_3
+    min_tls_version = var.zone_settings.min_tls_version
+    security_header {
+      enabled            = var.zone_settings.security_header.enabled
+      include_subdomains = var.zone_settings.security_header.include_subdomains
+      max_age            = var.zone_settings.security_header.max_age
+      nosniff            = var.zone_settings.security_header.nosniff
+      preload            = var.zone_settings.security_header.preload
+    }
+
+    http2 = var.zone_settings.http2
+    http3 = var.zone_settings.http3
+
+    brotli = var.zone_settings.brotli
+    minify {
+      css  = var.zone_settings.minify.css
+      js   = var.zone_settings.minify.js
+      html = var.zone_settings.minify.html
+    }
   }
 }
--- zone/variables.tf
+++ zone/variables.tf
@@ -1,3 +1,38 @@
 variable "dns_zone" {
   type = string
 }
+
+variable "zone_settings" {
+  type = object({
+    development_mode = string,
+
+    ipv6                  = string,
+    true_client_ip_header = string,
+
+    always_use_https         = string,
+    automatic_https_rewrites = string,
+    opportunistic_encryption = string,
+
+    ssl             = string,
+    security_level  = string,
+    tls_1_3         = string,
+    min_tls_version = string,
+    security_header = object({
+      enabled            = bool,
+      include_subdomains = bool,
+      max_age            = number,
+      nosniff            = bool,
+      preload            = bool,
+    }),
+
+    http2 = string,
+    http3 = string,
+
+    brotli = string,
+    minify = object({
+      css  = string,
+      js   = string,
+      html = string,
+    }),
+  })
+}
